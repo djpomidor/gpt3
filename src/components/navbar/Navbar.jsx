6db6bf607d68dc3a7de703e788fe8c9867646fa5
djpomidor <djpomidor@gmail.com>
2023-02-14 03:24:32 +0300

    start

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RiMenu3Line,RiCloseLine} from 'react-icons/ri';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='gpt3_navbar'>
+      <div className='gpt3__navbar-links'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
